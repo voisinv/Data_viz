6e07364836cf420803690b4733964d804e2f7327
Vincent Voisin <vincent.voisin@hotmail.fr>
2014-10-12 21:04:00 +0200

    Adding text to circles

--- app/scripts/directives/GraphDatas.js
+++ app/scripts/directives/GraphDatas.js
@@ -3,66 +3,58 @@ function graphDatas() {
 		restrict : 'A',
 		scope : true,
 		link: function(scope, iElem, iAttrs) {
-			var width = 1000;
-			var height = 700;
-			var color = d3.scale.category20c();
-			console.log(scope);
-			iElem.css('width', width);
-			iElem.css('height', height);
-			console.log(scope.main.tags)
-
-			iElem.css('border', 'solid black 1px')
+			var width = 1000,
+			    height = 700,
+	            elem = undefined;
 
 
 			 var force = d3.layout.force()
 				            .charge(-700)
+				            //.alpha(1)
+				            //.gravity(function(d){console.log(d.urls.length);return 1*d.urls.length})
 				            // à mettre pour la v2
 				            //.linkDistance(600)
 				            .size([width, height])
-				            .nodes(scope.main.tags)
-				            .start()
-				            ;
+				            .nodes(scope.main.tags);
 
-			scope.$watchCollection('$parent.main.tags', function(newVal, oldVal) {
-				if(angular.equals(newVal, oldVal)) return;
-				console.log('watch called');
-				displayData(); 
-			});
-	    	//d3.select(iElem[0]).remove('svg');
+
 	    	var svg = d3.select(iElem[0]).append('svg')
 	    			.attr("width", width)
 	        		.attr("height", height);
-            var displayData = function() {
-				var elem = svg.selectAll('circle')
-								.data(scope.main.tags);
-				console.log(scope.main.tags);
 
-				var nodes = elem.enter()
-					.append('circle')
+			scope.$watchCollection('$parent.main.tags', function(newVal, oldVal) {
+				if(angular.equals(newVal, oldVal)) return;
+				update(); 
+			});
+
+            var update = function() {
+				var elem = svg.selectAll('g').data(scope.main.tags);
+
+				var nodes = elem.enter().append('g')
+								.call(force.drag);
+
+				nodes.append('circle')
 					.attr('class', 'circle')
-					.attr('r', function(d){return d.urls.length * 40})
-					.on('click', function(d){console.log('click')});
+					.attr('r', function(d,i){return d.urls.length * 50;});
+
+				nodes.append('text')
+					.attr('class', 'text-circle')
+					.attr('font-size', function(d){return d.urls.length*20})
+					.attr('dx', function(d){return d.tagName.length * -5})
+					.attr('dy', '.2em')
+					.text(function(d){return d.tagName})
 
 				elem.call(force.drag);
-				/*
-				force.on('end', function() {
-					nodes
-                    .attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
-				});
-				*/
-				console.log('node');
-				console.log(nodes)
 
 				force.on("tick", function() {
-                    elem
-                    .attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
+					nodes.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
                 });
-				force.start();
-
+                console.log(force)
+            	force.start();
             }
-			displayData();
+			update();
+
+
 		} 
 	}
 }

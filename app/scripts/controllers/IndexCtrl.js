0f91579492c1ee68c6b9bf809d76d0ec223e9655
matt2mi <2m1tema@gmail.com>
2014-10-12 22:13:14 +0200

    new fct clickOnTag

--- app/scripts/controllers/IndexCtrl.js
+++ app/scripts/controllers/IndexCtrl.js
@@ -5,9 +5,16 @@ function IndexCtrl (DataValues) {
         tagName: '',
         url: ''
     };
+    vm.tagClicked = false;
 
     vm.addTag = function() {
         DataValues.addTag(vm.tag);
+    };
+
+    vm.clickOnTag = function(tag) {
+        angular.element('#container-dataviz-view').toggleClass('col-sm-12').toggleClass('col-sm-9');
+        angular.element('#container-tag-list').toggleClass('col-sm-3');
+        vm.tagClicked = true;
     };
 }
 

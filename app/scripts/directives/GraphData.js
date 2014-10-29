a9ce60a3dcc79b1f065442ce2a6705073f032cc4
matt2mi <2m1tema@gmail.com>
2014-10-30 00:15:51 +0100

    ajout fonctionnel mais recharge tout les cercles

--- app/scripts/directives/GraphData.js
+++ app/scripts/directives/GraphData.js
@@ -1,16 +1,19 @@
 function graphData($rootScope, $timeout) {
-	return {
-		restrict : 'A',
-		scope : true,
-		link: function(scope, elem, attrs) {
+    return {
+        restrict : 'A',
+        scope : true,
+        link: function(scope) {
             var w = 1280,
                 h = 800,
-                nodes = null;
+                nodes = null,
+                force = null,
+                color = d3.scale.category10();;
 
-            var nodes = scope.main.tags.map(function(d, i) { return {radius: _.size(d.urls) * 10, id: d.id}; }),
-                color = d3.scale.category10();
+            var svg = d3.select("#body").append("svg")
+                .attr("width", w)
+                .attr("height", h);
 
-            scope.$on('newTag', function(event) {
+            scope.$on('newTag', function() {
                 draw();
             });
 
@@ -21,84 +24,105 @@ function graphData($rootScope, $timeout) {
                 }
             });
 
-            var force = d3.layout.force()
-                .gravity(0.3)
-                .charge(function(d, i) { return -30 * d.radius; })
-                .nodes(nodes)
-                .size([w, h]);
+            function draw() {
+                nodes = scope.main.tags.map(function (d) {
+                        return {radius: _.size(d.urls) * 10, id: d.id};
+                    });
 
-            var root = nodes[0];
-            root.radius = 0;
-            root.fixed = true;
+                force = d3.layout.force()
+                    .gravity(0.3)
+                    .charge(function (d) {
+                        return -30 * d.radius;
+                    })
+                    .nodes(nodes)
+                    .size([w, h]);
 
-            force.start();
+                var root = nodes[0];
+                root.radius = 0;
+                root.fixed = true;
 
-            var svg = d3.select("#body").append("svg")
-                .attr("width", w)
-                .attr("height", h);
-
-            svg.selectAll("circle")
-                .data(nodes.slice(1))
-              .enter().append("circle")
-                .attr("r", function(d) { return d.radius-1; })
-                .attr("id", function(d) { return 'circle-' + d.id; })
-                .call(force.drag)
-                .on('mouseover', function(d) {
-                    d3.selectAll("circle").attr('opacity', 0.3);
-                    d3.select(this).attr('opacity', 1);
-                    $rootScope.$broadcast('hoverTag', d.id);
-                })
-                .on('mouseleave', function(d) { d3.selectAll("circle").attr('opacity', 1); })
-                .style("fill", function(d, i) { return color(i % _.size(scope.main.tags)); });
-
-            force.on("tick", function(e) {
-                var q = d3.geom.quadtree(nodes),
-                    i = 0,
-                    n = nodes.length;
-
-                while (++i < n) {
-                    q.visit(collide(nodes[i]));
-                }
+                force.start();
 
                 svg.selectAll("circle")
-                    .attr("cx", function(d) { return d.x; })
-                    .attr("cy", function(d) { return d.y; });
-            });
+                    .data(nodes.slice(1))
+                    .enter().append("circle")
+                    .attr("r", function (d) {
+                        return d.radius - 1;
+                    })
+                    .attr("id", function (d) {
+                        return 'circle-' + d.id;
+                    })
+                    .call(force.drag)
+                    .on('mouseover', function (d) {
+                        d3.selectAll("circle").attr('opacity', 0.3);
+                        d3.select(this).attr('opacity', 1);
+                        $rootScope.$broadcast('hoverTag', d.id);
+                    })
+                    .on('mouseleave', function () {
+                        d3.selectAll("circle").attr('opacity', 1);
+                    })
+                    .style("fill", function (d, i) {
+                        return color(i % _.size(scope.main.tags));
+                    });
 
-            function collide(node) {
-              var r = node.radius + 16,
-                  nx1 = node.x - r,
-                  nx2 = node.x + r,
-                  ny1 = node.y - r,
-                  ny2 = node.y + r;
-              return function(quad, x1, y1, x2, y2) {
-                if (quad.point && (quad.point !== node)) {
-                  var x = node.x - quad.point.x,
-                      y = node.y - quad.point.y,
-                      l = Math.sqrt(x * x + y * y),
-                      r = node.radius + quad.point.radius;
-                  if (l < r) {
-                    l = (l - r) / l * .5;
-                    node.x -= x *= l;
-                    node.y -= y *= l;
-                    quad.point.x += x;
-                    quad.point.y += y;
-                  }
+                force.on("tick", function () {
+                    var q = d3.geom.quadtree(nodes),
+                        i = 0,
+                        n = nodes.length;
+
+                    while (++i < n) {
+                        q.visit(collide(nodes[i]));
+                    }
+
+                    svg.selectAll("circle")
+                        .attr("cx", function (d) {
+                            return d.x;
+                        })
+                        .attr("cy", function (d) {
+                            return d.y;
+                        });
+                });
+
+                function collide(node) {
+                    var r = node.radius + 16,
+                        nx1 = node.x - r,
+                        nx2 = node.x + r,
+                        ny1 = node.y - r,
+                        ny2 = node.y + r;
+                    return function (quad, x1, y1, x2, y2) {
+                        if (quad.point && (quad.point !== node)) {
+                            var x = node.x - quad.point.x,
+                                y = node.y - quad.point.y,
+                                l = Math.sqrt(x * x + y * y),
+                                r = node.radius + quad.point.radius;
+                            if (l < r) {
+                                l = (l - r) / l * .5;
+                                node.x -= x *= l;
+                                node.y -= y *= l;
+                                quad.point.x += x;
+                                quad.point.y += y;
+                            }
+                        }
+                        return x1 > nx2
+                            || x2 < nx1
+                            || y1 > ny2
+                            || y2 < ny1;
+                    };
                 }
-                return x1 > nx2
-                    || x2 < nx1
-                    || y1 > ny2
-                    || y2 < ny1;
-              };
             }
 
             function resize(tagId) {
                 var node = d3.select('#circle-' + tagId)
-                             .attr('r', function(d) { return d.radius-1; });
-                             force.start();
+                    .attr('r', function (d) {
+                        return d.radius - 1;
+                    });
+                force.start();
+                draw();
             }
-		}
-	};
+
+            draw();
+        }
+    };
 }
 
 angular
